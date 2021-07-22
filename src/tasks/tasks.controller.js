ab94281bc70cf4f367df98a4c228be1a5cbb9124
Maryia Chepelava <4rontend@gmail.com>
2021-07-22 13:53:35 +0300

    feat: add servise

--- src/tasks/tasks.controller.js
+++ src/tasks/tasks.controller.js
@@ -1,38 +1,36 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const getAll = require("./tasks.service");
+const getOneTask = require("./tasks.service");
+const putOneTask = require('./tasks.service');
+const deleteOneTask = require('./tasks.service');
 
 const app = express();
 const jsonParser = express.json();
 const taskRouter = express.Router();
 
-app.get('/',function(req,res){
-    res.status(200);
-    res.send("<h1>Homepage</h2>");
-});
+
 taskRouter.get("/",function(req,res){
-    res.status(200);
-    res.send("Success");
+    getAll();
+    res.status(200).send("Success");
 });
-taskRouter.get("/:taskId", function(req,res){
-    res.status(200);
-    res.send("Success");
-    console.log(`taskId:  ${req.params.taskId}`);
+taskRouter.get("/:Id", function(req,res){
+    res.status(200).send("Success");
+    console.log(`Id:  ${req.params.Id}`);
+    getOneTask(req.params.Id);
 });
 
 taskRouter.post('/', jsonParser, function(req,res){
     console.log('Request.body: ',req.body);
-    res.status(200);
-    res.send("Success");
+    res.status(200).send("Success");
 }); 
-taskRouter.put('/:taskId',jsonParser, function(req,res){
-    console.log(`taskId:  ${req.params.taskId}`);
-    console.log("Title",req.body.title);
-    console.log("Description",req.body.discription);
+taskRouter.put('/:Id',jsonParser, function(req,res){
+    putOneTask(req.params.Id,req.body.title,req.body.discription);
     res.status(200).send("Success");
 });
 
-taskRouter.delete('/:taskId',jsonParser, function(req,res){
-    console.log(`taskId:  ${req.params.taskId}`);
+taskRouter.delete('/:Id',jsonParser, function(req,res){
+    deleteOneTask(req.params.Id);
     res.status(200).send("Success");
 });
 

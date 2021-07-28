6d8b7d8fb26ab03fe9bf58a9453751732c28de57
Maryia Chepelava <4rontend@gmail.com>
2021-07-28 14:44:38 +0300

    feat: add a Central Error Handling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const taskRouter = require('./tasks/tasks.controller');
+const { handleError, ErrorHandler } = require('./helpers/error');
 
 const app = express();
 const jsonParser = express.json();
@@ -9,6 +10,14 @@ app.use(express.json());
 app.get('/', (req,res) => {
     res.status(200);
     res.send('<h1>Homepage</h2>');
+});
+
+app.get('/error', (req, res) => {
+    throw new ErrorHandler(500, 'Internal server error');
+});  
+
+app.use((err, req, res, next) => {
+    handleError(err, res);
 });
 
 app.use('/tasks', taskRouter);
